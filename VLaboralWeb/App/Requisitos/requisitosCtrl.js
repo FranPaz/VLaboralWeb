dcbb872a448b13cffadf8bb4afd65e2dadd5858f
iafar86 <iafar86@hotmail.com>
2016-06-23 11:45:46 -0300

    *(habilidadesDF) GET - Llamar a la funcion del webApiController (habilidadesController), y se devolvera el listado de todas las Habilidades almacenados en la BD
    * (tipoRequisitoDataFactory) GET - Llamar a la funcion del webApiController (tipoRequisitoController), y se devolvera el listado de todos los tipos de Requisitos almacenados en la BD
    
    Archivos creados : habilidadesCtrl, habilidadesDF, requisitosCtrl, requisitosDF

---
+++ VLaboralWeb/App/Requisitos/requisitosCtrl.js
@@ -0,0 +1,7 @@
+/**
+ * Created by ifarhat on 23/06/2016.
+ */
+vLaboralApp.controller('requisitosCtrl', function ($scope, requisitosDf, prueba) {
+    $scope.prueba = prueba;
+    $scope.user = {};
+});
