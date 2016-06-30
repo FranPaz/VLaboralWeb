942daaf63c4ba528e879321b0c4db833d19d1cda
iafar86 <iafar86@hotmail.com>
2016-06-30 20:36:20 -0300

    Funcion Put en profesionalesDF

--- VLaboralWeb/App/Profesionales/profesionalesDF.js
+++ VLaboralWeb/App/Profesionales/profesionalesDF.js
@@ -16,10 +16,24 @@
         return deferred.promise;
     }
 
+    var _putProfesional = function (prmId, data) { // iafar: Modifica un Profesional segun Id
+        var deferred = $q.defer();
+
+        $http.put(urlApi + 'api/Profesionals/' + prmId, data).then(
+            function (response) {
+                deferred.resolve(response);
+            },
+
+            function (response) {
+                deferred.reject(response.data);
+            });
+        return deferred.promise;
+    };
 
 
     //#region iafar: area de asignacion de funciones a objeto
     profesionalesDF.getProfesional = _getProfesional;
+    profesionalesDF.putProfesional = _putProfesional;
     //#endregion
 
 
