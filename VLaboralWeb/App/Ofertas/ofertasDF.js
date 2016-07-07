d2cbce29c32fe49bfa631844de28a5faf802ef9e
iafar86 <iafar86@hotmail.com>
2016-07-07 15:58:00 -0300

    agregado funcion GET con objeto busqueda en ofertadf

--- VLaboralWeb/App/Ofertas/ofertasDF.js
+++ VLaboralWeb/App/Ofertas/ofertasDF.js
@@ -4,6 +4,19 @@
     //var urlApi = ""; //iafar: url azure a definir
     var ofertasDF = {};
 
+    var _getOferta = function (prmObjBusqueda) { //iafar: Traer todas las ofertas segun objeto de busqueda
+        var deferred = $q.defer();
+        $http.post(urlApi + '/api/Ofertas/' + prmObjBusqueda).then(
+            function (response) {
+                deferred.resolve(response);
+            },
+            function (response) {
+                deferred.reject(response.data);
+            });
+        return deferred.promise;
+    };
+
+
     var _postOferta = function (data) { //iafar: alta de una oferta en particular
         var deferred = $q.defer();
         $http.post(urlApi + '/api/Ofertas/', data).then(
@@ -20,6 +33,7 @@
 
 
     //#region iafar: area de asignacion de funciones a objeto
+    ofertasDF.getOferta = _getOferta;
     ofertasDF.postOferta = _postOferta;
     //#endregion
 
