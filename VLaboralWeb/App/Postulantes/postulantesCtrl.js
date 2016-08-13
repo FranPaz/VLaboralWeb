f1ecd8bd35375d7895eb918b1f726a3891af7f34
iafar86 <iafar86@hotmail.com>
2016-08-13 16:33:41 -0300

    Crear el datafactory postulacionDF
    (postulacionDF.POST) POST(prmPostulacion) - llamar a la funcion del web api controller postulacionController.POST para cargar una nueva postulacion, en caso de que la postulacion no se pueda hacer informar al usuario, tanto en caso de error como en caso de que no cumpla con los requisitos obligatorios

---
+++ VLaboralWeb/App/Postulantes/postulantesCtrl.js
@@ -0,0 +1,6 @@
+﻿vLaboralApp.controller('postulantesCtrl', function ($scope, $mdMedia, $mdDialog, //iafar: definicion de inyectores de dependencias
+    postulantesDF, authSvc, //iafar: definicion de data factorys
+     listadoPostulantes//iafar: definicion de parametros de entrada 
+    ) {
+
+});
