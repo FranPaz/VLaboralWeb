e3e923e51dcfcf47f23ab22f4ee7c0a4c17c371c
franpaz <franfpaz@gmail.com>
2016-09-27 21:41:01 -0300

    agrego estructura para notificaciones

---
+++ VLaboralWeb/App/Notificaciones/notificacionesDF.js
@@ -0,0 +1,26 @@
+﻿vLaboralApp.factory('notificacionesDF', function ($http, $q, configSvc) {
+
+    var urlApi = configSvc.urlApi; // fpaz: toma el url del api de configSvc
+    var notificacionesDF = {};
+
+    //var _postExperiencia = function (data) { //iafar: alta de una Experiencia Academica en particular
+    //    var deferred = $q.defer();
+    //    $http.post(urlApi + 'api/ExperienciaLaborals', data).then(
+    //        function (response) {
+    //            deferred.resolve(response);
+    //        },
+    //        function (response) {
+    //            deferred.reject(response.data);
+    //        });
+    //    return deferred.promise;
+    //};
+
+ 
+
+    //#region iafar: area de asignacion de funciones a objeto
+    //experienciasLaboralesDF.postExperiencia = _postExperiencia;
+    //experienciasLaboralesDF.putExperiencia = _putExperiencia;
+    //#endregion
+
+    return notificacionesDF;
+});
