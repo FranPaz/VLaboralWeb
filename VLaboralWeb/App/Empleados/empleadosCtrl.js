b2012316adcf597f16a9cd6375ada87a2746cd7d
franpaz <franfpaz@gmail.com>
2017-04-11 21:31:10 -0300

    agrego listado de empleados

--- VLaboralWeb/App/Empleados/empleadosCtrl.js
+++ VLaboralWeb/App/Empleados/empleadosCtrl.js
@@ -1,9 +1,26 @@
-﻿vLaboralApp.controller('empleadosCtrl', function ($scope, $mdMedia, $mdDialog, $ocLazyLoad,
-    empleados, authSvc) {
+﻿vLaboralApp.controller('empleadosCtrl', function ($scope, $mdMedia, $mdDialog, $ocLazyLoad    
+    , authSvc, empleadosDF //fpaz: definicion de data factorys
+    , empleados, listOpcionesFiltrosEmpleados//fpaz: definicion de parametros de entrada    
+    ) {
 
+    //#region fpaz: Inicializacion de variables de Scope
     $scope.empleados = empleados;
+    $scope.opcionesFiltrosEmpleados = listOpcionesFiltrosEmpleados;
 
-    //#region empleados
+    //#region variables iniciales para paginacion
+    $scope.queryFiltros = { // tiene los parametros que voy a pasar para filtrar el listado. 
+        Rows: 5, //valor inicial de cantidad de filas mostradas
+        Page: 1 //valor inicial de la pagina mostrada
+    };
+
+    $scope.limitOptions = [1, 2, 5, 10, 15];// sirve para seleccionar la cantidad de filas que se van a mostrar en la tabla
+
+    $scope.TotalRows;
+    //#endregion
+
+    //#endregion
+
+    //#region alta empleados
     $scope.nuevoEmpleado = function () {
         var useFullScreen = ($mdMedia('sm') || $mdMedia('xs')) && $scope.customFullscreen;
         $mdDialog.show({
@@ -29,6 +46,70 @@
         });
     }
 
+    //#endregion
+
+    //#region fpaz: funciones para el manejo de filtros y ordenamiento en el listado de empleados
+    $scope.setSelectedEmpleados = function (filterType, filterValue) { //funcion para armar el objeto con los filtros para los empleados
+        //si el valor pasado como parametro existe lo elimino, sino lo agrego
+
+        //convierto el id en string
+        var id = filterValue.Id.toString();
+
+        if ($scope.queryFiltros[filterType] && $scope.queryFiltros[filterType].indexOf(id) >= 0) {
+            if (Array.isArray($scope.queryFiltros[filterType])) {
+                $scope.queryFiltros[filterType].splice($scope.queryFiltros[filterType].indexOf(id), 1);
+            } else {
+                $scope.queryFiltros[filterType] = [];
+            }
+        } else {
+            if (!$scope.queryFiltros[filterType]) {
+                $scope.queryFiltros[filterType] = [];
+            }
+
+            $scope.queryFiltros[filterType].push(id);
+        }
+
+        $scope.obtenerListadoFiltradoEmpleados();
+
+    }
+
+    $scope.obtenerListadoFiltradoEmpleados = function () {
+        console.log('entra a $scope.obtenerListadoFiltradoEmpleados');
+
+        var query = angular.copy($scope.queryFiltros);
+
+        empleadosDF.obtenerEmpleadosFiltrados(query).then(function (response) {
+            console.log('entra a obtener empleados filtrados');
+            $scope.empleados = response.results;
+            $scope.TotalRows = response.totalPages;
+        },
+    function (err) {
+        if (err) {
+            $scope.error = err;
+            alert("Error al Filtrar los empleados: " + $scope.error.Message);
+        }
+    });
+    }
+
+    $scope.logPaginationEmpleados = function (page, limit) {
+        console.log('page: ', page);
+        console.log('limit: ', limit);
+        //llamo al webapi para obtener los valores filtrados        
+        $scope.obtenerListadoFiltradoempleados();
+    }
+
+    $scope.ordenEmpleadosChanged = function () {
+        console.log('Ordenamiento: ', $scope.queryFiltros.orderBy);
+        //llamo al webapi para obtener los valores filtrados        
+        $scope.obtenerListadoFiltradoEmpleados();
+    }
+
+    $scope.busquedaNombreEmpleado = function (prmNombre) {
+        console.log('Busqueda por nombre: ', $scope.queryFiltros.searchText);
+        $scope.queryFiltros.searchText = prmNombre;
+        //llamo al webapi para obtener los valores filtrados        
+        $scope.obtenerListadoFiltradoEmpleados();
+    }
     //#endregion
 
 });
--- VLaboralWeb/App/Empleados/empleadosDF.js
+++ VLaboralWeb/App/Empleados/empleadosDF.js
@@ -1,5 +1,82 @@
-﻿vLaboralApp.factory('empresasDF', function ($http, $q, configSvc) {
+﻿vLaboralApp.factory('empleadosDF', function ($http, $q, configSvc) {
+
+    //iafar: url del web api de cuentas de usuario, cambiar por el de produccion una vez implementado
+    var urlApi = configSvc.urlApi; //desarrollo    
+    var empleadosDF = {};
+
+    var _getEmpleados = function (page, rows) { 
+        var deferred = $q.defer();
+        $http.get(urlApi + 'api/Empleados/', {
+            params: {
+                page: page,
+                rows: rows
+            }
+        }).then(
+           function (response) {
+               deferred.resolve(response.data.Results);
+           },
+            function (response) {
+                deferred.reject(response.data);
+            });
+        return deferred.promise;
+    }
+
+    var _getEmpleado = function (prmId) {
+        var deferred = $q.defer();
+        $http.get(urlApi + 'api/Empleados/' + prmId).then(
+            function (response) {                
+                deferred.resolve(response.data);
+            },
+            function (response) {
+                deferred.reject(response.data);
+            });
+        return deferred.promise;
+    }
+    
+    //#region fpaz: obtiene el listado de filtros disponibles y posibles valores de esos filtros
+    var _obtenerOpcionesFiltrosEmpleados = function () {
+        var deferred = $q.defer();
+        var options = {
+            Filters: [
+                "Rubros",
+                "Valoraciones"
+            ]
+        };
+
+        $http.post(urlApi + 'api/Empleados/QueryOptions', options).then(
+            function (response) {
+                deferred.resolve(response.data);
+            },
+            function (response) {
+                deferred.reject(response.data);
+            });
+        return deferred.promise;
+    };
+    //#endregion
+
+    //#region fpaz: devuelve el listado de empleados filtrados
+    var _obtenerEmpleadosFiltrados = function (prmQueryBusquedaFiltrada) {
+        var deferred = $q.defer();
+
+        $http.post(urlApi + 'api/Empleados/Search', prmQueryBusquedaFiltrada).then(
+            function (response) {
+                deferred.resolve(response.data);
+            },
+            function (response) {
+                deferred.reject(response.data);
+            });
+        return deferred.promise;
+    };
+    //#endregion
+
+    //#region iafar: area de asignacion de funciones a objeto
+    empleadosDF.getEmpleados = _getEmpleados;
+    empleadosDF.getEmpleado = _getEmpleado;
+    empleadosDF.obtenerOpcionesFiltrosEmpleados = _obtenerOpcionesFiltrosEmpleados;
+    empleadosDF.obtenerEmpleadosFiltrados = _obtenerEmpleadosFiltrados;
+    //#endregion
 
 
+    return empleadosDF;
 
 })
